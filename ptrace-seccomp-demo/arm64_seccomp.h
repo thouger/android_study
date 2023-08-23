5a1dd7253a44ac3115b8515113d491f26b4b2aab
thouger <1030490158@qq.com>
2023-08-23 15:55:10 +0800

    增加一个ptrace+seccomp拦截syscall的demo

---
+++ ptrace-seccomp-demo/arm64_seccomp.h
@@ -0,0 +1,31 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/ptrace.h>
+#include <sys/user.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <errno.h>
+
+static void process_signals(pid_t child);
+static int wait_for_open(pid_t child);
+static void read_file(pid_t child, char *file,user_pt_regs regs);
+static void redirect_file(pid_t child, const char *file,user_pt_regs regs);
+void putdata(pid_t pid, uint64_t addr, char * str, long sz);
+
+
+#if defined(__aarch64__)
+	#define ARM_x0 regs[0]
+    #define ARM_x1 regs[1]
+    #define ARM_x2 regs[2]
+	#define ARM_x8 regs[8]
+	#define ARM_lr regs[30]
+	#define ARM_sp sp
+	#define ARM_pc pc
+	#define ARM_cpsr pstate
+	#define NT_PRSTATUS 1
+	#define NT_foo 1
+#endif
